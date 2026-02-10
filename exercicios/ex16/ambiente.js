64e6b0b3245092ad3c01309222a7cee59ed3aa11
ArthurTomaz <tomazarthur410@gmail.com>
2026-02-09 22:30:54 -0300

    voltando os estudos

---
+++ exercicios/ex16/ambiente.js
@@ -0,0 +1,29 @@
+for (var c = 1; c <= 6; c++){
+    console.log("tudo bem?")
+}
+
+
+/*var c = 1
+do{
+    console.log("tudo bem?")
+    c++ // c++ = c + 1
+} while(c <= 6)
+*/
+
+/*
+var c = 1
+while (c <= 6){
+    console.log("tudo bem?")
+    c++ // c++ = c + 1
+}
+*/
+
+
+/*
+console.log("tudo bem?")
+console.log("tudo bem?")
+console.log("tudo bem?")
+console.log("tudo bem?")
+console.log("tudo bem?")
+console.log("tudo bem?")
+*/
